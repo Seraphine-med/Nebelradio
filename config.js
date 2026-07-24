83a7883691ba4c2aee9fe166764c6716f2c974d3
Seraphine-med <seraphinemeadows@seznam.cz>
2026-07-24 14:25:34 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-const ACTIVE_RADIO = "template-radio";
-// const ACTIVE_RADIO = "dusicky-radio";
+const ACTIVE_RADIO = "dusicky-radio";
+
+// const ACTIVE_RADIO = "template-radio";
 // const ACTIVE_RADIO = "halloween-radio";
 // const ACTIVE_RADIO = "lockhart-radio";
