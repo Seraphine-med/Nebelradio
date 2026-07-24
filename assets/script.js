faf09e7ebba2ce79b2a0f0d40090af4e44b0facd
Seraphine-med <seraphinemeadows@seznam.cz>
2026-07-24 08:24:17 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -23,7 +23,7 @@ let durations = [];
 
 
 // čas začátku vysílání
-const broadcastStart = new Date("2026-07-23T8:20:00");
+const broadcastStart = new Date("2026-07-24T8:20:00");
 
 fetch("playlist.json")
 
