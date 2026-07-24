cb2ad3ba2327b881ad5d19578210beda50bc3750
Seraphine-med <seraphinemeadows@seznam.cz>
2026-07-24 14:37:58 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -28,7 +28,7 @@ let durations = [];
 const startTime = document.body.dataset.start;
 const broadcastStart = new Date(startTime);
 
-fetch(`radia/${ACTIVE_RADIO}/playlist.json`)
+fetch("playlist.json")
 
 .then(response => response.json())
 
@@ -75,7 +75,7 @@ function loadSong(){
 
     const song = playlist[currentSong];
 
-    audio.src = `radia/${ACTIVE_RADIO}/${song.url}`;
+    audio.src = song.url;
 audio.load();
     
 }
