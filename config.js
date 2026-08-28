bf93f42a8312e3f7828f8e8d5d75238f2b3485f5
Seraphine-med <seraphinemeadows@seznam.cz>
2026-08-28 15:23:15 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 // Aktivní rádio pro návštěvníky webu
-const ACTIVE_RADIO = "lockhart-radio";
+const ACTIVE_RADIO = "snehohratky-radio";
 
 // Dostupná rádia:
 // template-radio
 // dusicky-radio
 // halloween-radio
 // lockhart-radio
+// snehohratky-radio
